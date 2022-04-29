55d6aa2dd702af9db6b0e81af8e3b2ad65c78a27
Rozsnyai Tamas <rozsthomas@gmail.com>
2022-04-29 12:12:54 +0300

    Required annotation added to DTO fields

--- RedRock.Calendar.Modules.Users.Contract/UserDTO.cs
+++ RedRock.Calendar.Modules.Users.Contract/UserDTO.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RedRock.Calendar.Modules.Users.Contract
 {
     public class UserDTO
     {
+        [Required()]
         public Guid Id { get; set; }
+        [Required()]
         public string FullName { get; set; }
+        [Required()]
         public string UserName { get; set; }
     }
 }
