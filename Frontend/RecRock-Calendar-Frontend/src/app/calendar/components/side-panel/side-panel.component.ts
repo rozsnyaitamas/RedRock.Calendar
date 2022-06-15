39593102e03adcecded72a20d922e23ce263e9e0
Rozsnyai Tamas <rozsthomas@gmail.com>
2022-06-15 15:11:55 +0300

    minor changes

--- Frontend/RecRock-Calendar-Frontend/src/app/calendar/components/side-panel/side-panel.component.ts
+++ Frontend/RecRock-Calendar-Frontend/src/app/calendar/components/side-panel/side-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FinanceDTO } from '@redrock/generated-html-client/models';
 import { FinanceService } from '@redrock/services/finance.service';
 import { StorageHelper } from '@redrock/shared/helpers/storage.helper';
@@ -6,31 +6,32 @@ import { StorageHelper } from '@redrock/shared/helpers/storage.helper';
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
-  styleUrls: ['./side-panel.component.scss']
+  styleUrls: ['./side-panel.component.scss'],
 })
 export class SidePanelComponent implements OnInit {
-
   @Input() viewDate: Date = new Date();
   financeDTOs: FinanceDTO[] = [];
   overallSum: number = 0;
+  @ViewChild('feesContent') feesContent!: ElementRef;
 
-  constructor(private readonly financeService: FinanceService) { }
+  constructor(private readonly financeService: FinanceService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getFee(): void {
     var userId = StorageHelper.getUserId(sessionStorage);
-    if(userId){
-      this.financeService.getMonthlyFee(userId, this.viewDate).then(
-        (financeDTOs) => {
+    if (userId) {
+      this.financeService
+        .getMonthlyFee(userId, this.viewDate)
+        .then((financeDTOs) => {
           this.financeDTOs = financeDTOs;
-          financeDTOs.forEach(financeDTO => {
+          financeDTOs.forEach((financeDTO) => {
             this.overallSum += financeDTO.sum;
           });
-        }
-      );
+        });
     }
   }
 
+  exportPdf() {
+  }
 }
